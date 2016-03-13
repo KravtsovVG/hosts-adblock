f965f94781274e14865c050600a051ef1e51832f
Daniel Schömer <daniel.schoemer@gmx.net>
2016-03-13 18:01:46 +0100

    Test for required commands, more comments

--- hosts-adblock.sh
+++ hosts-adblock.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 
+# dnsmasq configuration file
+# $1 or /etc/dnsmasq.conf
 export DNSMASQ_CONFIG="${1:-/etc/dnsmasq.conf}"
 
+# directory of this script
 export DIR="$(dirname "$0")"
+# directory to write the processed hosts files for dnsmasq
 export HOSTS_DIR="${DIR}/hosts.d"
+# IPv4 address to resolve ad-blocked hostnames to
+# Do not use 127.0.0.1 if your device is running a local webserver
 export IP4_ADDR="255.255.255.255"
+# IPv6 address to resolve ad-blocked hostnames to
+# Do not use ::1 if your device is running a local webserver
 export IP6_ADDR="ffff::ffff"
 
+# URLs to download ad-block hosts files
 export URLS=""
 export URLS="${URLS} https://adaway.org/hosts.txt"
 export URLS="${URLS} http://winhelp2002.mvps.org/hosts.txt"
@@ -14,13 +23,38 @@ export URLS="${URLS} http://hosts-file.net/ad_servers.txt"
 export URLS="${URLS} https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext"
 export URLS="${URLS} https://www.malwaredomainlist.com/hostslist/hosts.txt"
 
+
+# _test_for_required_commands
+#
+# Test if the required commands are available.
+_test_for_required_commands() {
+    export REQUIRED_CMDS="awk curl sed tr"
+    export REQUIRED_CMDS_FOUND=yes
+
+    for REQUIRED_CMD in $REQUIRED_CMDS ; do
+        if ! type "${REQUIRED_CMD}" >/dev/null 2>&1 ; then
+            REQUIRED_CMDS_FOUND=no
+            _log "$0 needs ${REQUIRED_CMD} available in \$PATH"
+        fi
+    done
+
+    if [ "${REQUIRED_CMDS_FOUND}" == "yes" ] ; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+# _append_config CONTENT FILENAME
+#
+# Add configuration line CONTENT to FILENAME.
 _append_config() {
     local CONTENT="$1"
-    local CONFIG="$2"
+    local FILENAME="$2"
 
-    if ! grep -q "${CONTENT}" "${CONFIG}" ; then
-        _log "Adding ${CONTENT} to ${CONFIG}"
-        echo "${CONTENT}" >>"${CONFIG}"
+    if ! grep -q "${CONTENT}" "${FILENAME}" ; then
+        _log "Adding ${CONTENT} to ${FILENAME}"
+        echo "${CONTENT}" >>"${FILENAME}"
     fi
 }
 
@@ -42,7 +76,7 @@ _download() {
 # Write MESSAGE to syslog.
 _log() {
     echo "$1"
-    # logger -t "$0" "$1"
+    type logger >/dev/null && logger -t "$0" "$1"
 }
 
 # _process FILENAME
@@ -72,6 +106,8 @@ _url_filename() {
     sed -e 's|?.*||g' -e 's|[.:/\#*+"(){}]\+|_|g' |
     tr '[A-Z]' '[a-z]'
 }
+
+_test_for_required_commands || exit $?
 
 cd "${DIR}"
 
